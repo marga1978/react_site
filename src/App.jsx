96c69790362827df09602e37a7530e78641355e4
marga1978 <marco.galante78@gmail.com>
2025-01-25 21:05:55 +0100

    site-featire

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import {Navbar} from './components/Navbar';
 import Home from './components/Home';
+import { Features } from './components/Features';
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       
       <Navbar />
       <Home />
+      <Features />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
         "secondary":"#9a7af1",
         "tartiary":"#707070",
         "pink":"#ee9ae5",
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       }
     },
   },
